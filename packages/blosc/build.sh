4d6b33c0480726ffa6e712c1e9da9d4fbe1804ba
T-Dynamos <anshdadwal298@gmail.com>
2022-09-28 17:00:25 +0530

    add(main): new package blosc

---
+++ packages/blosc/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.blosc.org
+TERMUX_PKG_DESCRIPTION="A blocking, shuffling and loss-less compression library"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.21.1
+TERMUX_PKG_SRCURL=https://github.com/Blosc/c-blosc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f387149eab24efa01c308e4cba0f59f64ccae57292ec9c794002232f7903b55b
+TERMUX_PKG_DEPENDS="libsnappy, zstd"
